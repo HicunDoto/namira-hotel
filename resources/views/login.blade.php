cc00668e02916a1b127a6b72f194464e218b92fb
HicunDoto <jekrai8@gmail.com>
2022-11-13 19:05:35 +0700

    Hicun 13/11/2022 19:05

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,28 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Admin Namira</title>
-</head>
-<body>
-    <form action="{{url('/login34N4mira')}}" method="post">
-        @csrf
-        @if ($message = Session::get('failed'))
-        <div class="alert alert-danger">
-            <strong>{{ $message }}</strong>
+@extends('head')
+    <div class="kotak-login d-flex flex-column align-items-center justify-content-center">
+        <img src="{{asset('gambar/logo-area-1.png')}}" class="logo-login mb-4">
+        <div class="kotak-rounded d-flex flex-column align-items-center text-center">
+            <h1 class="text-white mb-3">Login Admin Namira</h1>
+            <form action="{{url('/login34N4mira')}}" method="post">
+                @csrf
+                @if ($message = Session::get('failed'))
+                <div class="alert alert-danger">
+                    <strong>{{ $message }}</strong>
+                </div>
+                @endif
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Email</span>
+                    <input type="email" class="form-control" name="email" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                </div>
+                <div class="input-group mb-1">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Password</span>
+                    <input type="password" class="form-control pswd" name="password" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                </div>
+                <div class="form-check text-white" style="width:100%;text-align:left">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                    <label class="form-check-label" for="flexCheckChecked" style="">
+                        Lihat Password
+                    </label>
+                </div>
+                <a href=""><button class="btn btn-warning mt-3" type="submit">Login</button></a>
+            </form>
         </div>
-        @endif
-        <div>
-            <div style="padding-bottom: 5px;">
-                <input type="email" placeholder="Username" name="email">
-            </div>
-            <div style="padding-bottom: 5px;">
-                <input type="password" placeholder="Judul" name="password">
-            </div>
-            <button type="submit">Submit</button>
-        </div>
-    </form>
-</body>
-</html>
+    </div>
